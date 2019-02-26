ba553c3914682e1772c8db3b154ac9f4c6e9cda9
Ayce Ciloglugil <ayceciloglugil@gmail.com>
2019-02-26 17:26:13 +0000

    mongo integration test

--- src/test/java/ayc/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/ayc/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -4,21 +4,41 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ayc.recipe.bootstrap.RecipeBootstrap;
 import ayc.recipe.model.UnitOfMeasure;
-import ayc.recipe.repositories.UnitOfMeasureRepository;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 	
 	@Autowired //we have spring context since @RunWith(SpringRunner.class) so we can autowired
 	UnitOfMeasureRepository uomr;
+	
+	@Autowired
+	RecipeRepository reciper;
+	
+	@Autowired
+	CategoryRepository categoryr;
+	
+	
+	@Before
+	public void setUp() {
+		//since running before every test, duplication error is rise.
+		reciper.deleteAll();
+		uomr.deleteAll();
+		categoryr.deleteAll();
+		
+		//for initialization data
+		RecipeBootstrap recipeBootstrap = new RecipeBootstrap(reciper, uomr, categoryr);
+		recipeBootstrap.onApplicationEvent(null);
+	}
 	
 	@Test
 	public void findByUom() {
